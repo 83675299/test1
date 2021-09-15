529990ab74d216e553345e531668e0e44254efbe
83675299 <60167338+83675299@users.noreply.github.com>
2021-09-15 18:51:34 +0800

    Update main.py
    进行代码检查，把所有的警告消除了

--- 031902505/main.py
+++ 031902505/main.py
@@ -1,4 +1,3 @@
-import json
 import pypinyin
 
 
@@ -12,72 +11,76 @@ class Node(object):
         self.isend = False
 
 
+def ac_build(file):
+    # 读入文件
+    with open(file, encoding='UTF-8') as words:
+        words = words.read().split('\n')
+    # 建树
+    root = Node()
+    for word in words:
+        p = root
+        first = 0
+        for i in range(0, len(word)):
+            k = word[i]
+            f = False  # 来判断下一个是否是汉字
+            if '\u4e00' <= word[i] <= '\u9fff':
+                for x in pypinyin.pinyin(word[i], style=pypinyin.NORMAL):  # 把汉字变成拼音
+                    k = ''.join(x)
+            if i < len(word) - 1 and '\u4e00' <= word[i + 1] <= '\u9fff':  # 找下一个汉字来解决首字母的问题（成功了一部分）
+                for x in pypinyin.pinyin(word[i + 1], style=pypinyin.NORMAL):  # 把汉字变成拼音
+                    u = ''.join(x)
+                    first = u[0]
+                    f = True
+
+            for j in range(0, len(k)):
+                if k[j] in p.next.keys():
+                    p = p.next[k[j]]
+                    if j == 0 and f is True:
+                        p.next[first] = Node(first)
+                    if j == 0 and i == len(word) - 1:
+                        p.isend = True
+                    continue
+                else:
+                    p.next[k[j]] = Node(k[j])
+                p = p.next[k[j]]
+                if j == 0 and f is True:  # 还是解决首字母
+                    p.next[first] = Node(first)
+                if j == 0 and i == len(word) - 1:
+                    p.isend = True
+        p.word = word
+        p.isend = True
+    return root
+
+
+def fail_build(root):
+    # 使用fail指向来做AC机
+    queue = []
+    queue.insert(0, (root, None))
+    while len(queue) > 0:
+        node_parent = queue.pop()
+        temp, parent = node_parent[0], node_parent[1]
+        for i in temp.next.values():
+            queue.insert(0, (i, temp))
+        if parent is None:
+            continue
+        elif parent is root:
+            temp.fail = root
+        else:
+            fail = parent.fail
+            while fail and temp.value not in fail.next:
+                fail = fail.fail
+            if fail:
+                temp.fail = fail.next[temp.value]
+            else:
+                temp.fail = root
+    root.fail = root
+    return root
+
+
 class AC(object):
     def __init__(self, words):
-        self.root = AC.ac_build(self, words)
-
-    def ac_build(self, file):
-        # 读入文件
-        with open(file, encoding='UTF-8') as words:
-            words = words.read().split('\n')
-        # 建树
-        root = Node()
-        p = Node()
-        for word in words:
-            p = root
-            length=len(word)
-            for i in range(0,len(word)):
-                k=word[i]
-                f=False#来判断下一个是否是汉字
-                if '\u4e00' <= word[i] <= '\u9fff':
-                    for x in pypinyin.pinyin(word[i], style=pypinyin.NORMAL):  # 把汉字变成拼音
-                        k = ''.join(x)
-                if i <len(word)-1 and '\u4e00' <= word[i+1] <= '\u9fff':#找下一个汉字来解决首字母的问题（成功了一部分）
-                    for x in pypinyin.pinyin(word[i+1], style=pypinyin.NORMAL):  # 把汉字变成拼音
-                        u = ''.join(x)
-                        first=u[0]
-                        f = True
-
-                for j in range(0, len(k)):
-                    if k[j] in p.next.keys():
-                        p = p.next[k[j]]
-                        if j == 0 and f == True:
-                            p.next[first] = Node(first)
-                        if j == 0 and i == len(word) - 1:
-                            p.isend = True
-                        continue
-                    else:
-                        p.next[k[j]] = Node(k[j])
-                    p = p.next[k[j]]
-                    if j==0 and f==True:#还是解决首字母
-                        p.next[first]=Node(first)
-                    if j==0 and i==len(word)-1:
-                        p.isend=True
-            p.word = word
-            p.isend = True
-        # 使用fail指向来做AC机
-        queue = []
-        queue.insert(0, (root, None))
-        temp = None
-        while len(queue) > 0:
-            node_parent = queue.pop()
-            temp, parent = node_parent[0], node_parent[1]
-            for i in temp.next.values():
-                queue.insert(0, (i, temp))
-            if parent is None:
-                continue
-            elif parent is root:
-                temp.fail = root
-            else:
-                fail = parent.fail
-                while fail and temp.value not in fail.next:
-                    fail = fail.fail
-                if fail:
-                    temp.fail = fail.next[temp.value]
-                else:
-                    temp.fail = root
-        root.fail = root
-        return root
+        self.root = ac_build(words)
+        self.root = fail_build(self.root)
 
     def search(self, file):
         with open(file, encoding='UTF-8') as words:
@@ -86,20 +89,21 @@ class AC(object):
         out2 = []
         out3 = []
         count = 0
+        begin = 0
+        j = 0
         for i in range(0, len(words)):
             word = words[i]
             p = self.root
             for j in range(0, len(word)):
-                ischinese = False
                 k = word[j]
                 if p == ac.root:
                     begin = j
                 if '\u4e00' <= k <= '\u9fff':
                     for x in pypinyin.pinyin(k, style=pypinyin.NORMAL):  # 把汉字拆成拼音，对单个字母检索
                         k = ''.join(x)
-                        for l in range(0, len(k)):
-                            if k[l] in p.next.keys():
-                                p = p.next[k[l]]
+                        for t in range(0, len(k)):
+                            if k[t] in p.next.keys():
+                                p = p.next[k[t]]
                             elif p.isend is True:
                                 out1.append(i)
                                 out2.append(p.word)
@@ -108,13 +112,13 @@ class AC(object):
                                 count += 1
                             else:
                                 p = p.fail
-                                if k[l] in p.next.keys():
-                                    p = p.next[k[l]]
+                                if k[t] in p.next.keys():
+                                    p = p.next[k[t]]
                     continue
                 elif 'A' <= k <= 'Z':
                     k = k.lower()
                 elif 'a' <= k <= 'z':
-                    None
+                    pass
                 elif p.isend is True:
                     out1.append(i)
                     out2.append(p.word)
@@ -124,13 +128,13 @@ class AC(object):
                 else:
                     continue
 
-                if p.isend is True:#是否结束，因为有很多情况，所以多复制了几次判断
+                if p.isend is True:  # 是否结束，因为有很多情况，所以多复制了几次判断
                     out1.append(i)
                     out2.append(p.word)
                     out3.append(word[begin:j])
                     p = self.root
                     count += 1
-                elif k in p.next.keys():#进入下个结点
+                elif k in p.next.keys():  # 进入下个结点
                     p = p.next[k]
                 else:
                     p = p.fail
@@ -141,10 +145,8 @@ class AC(object):
             if p.isend is True:
                 out1.append(i)
                 out2.append(p.word)
-                out3.append(word[begin:j+1])
-                p = self.root
+                out3.append(word[begin:j + 1])
                 count += 1
-            p = self.root
 
         print(f"total : {count}")
         for i in range(0, count):
